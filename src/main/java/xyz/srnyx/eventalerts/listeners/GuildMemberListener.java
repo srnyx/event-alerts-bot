80954cea6edd760811a003e9b42c2428d136df5d
srnyx <25808801+srnyx@users.noreply.github.com>
2023-09-21 17:42:19 -0400

    Fix booster passes retaining after boost ends

---
+++ src/main/java/xyz/srnyx/eventalerts/listeners/GuildMemberListener.java
@@ -0,0 +1,24 @@
+package xyz.srnyx.eventalerts.listeners;
+
+import org.jetbrains.annotations.NotNull;
+
+import xyz.srnyx.eventalerts.EventAlerts;
+import xyz.srnyx.eventalerts.mongo.objects.Booster;
+
+import xyz.srnyx.lazylibrary.LazyListener;
+import xyz.srnyx.lazylibrary.events.GuildMemberStopBoost;
+
+
+public class GuildMemberListener extends LazyListener {
+    @NotNull private final EventAlerts eventAlerts;
+
+    public GuildMemberListener(@NotNull EventAlerts eventAlerts) {
+        this.eventAlerts = eventAlerts;
+    }
+
+    @Override
+    public void onGuildMemberStopBoosting(@NotNull GuildMemberStopBoost event) {
+        final Booster booster = eventAlerts.mongo.boosterCollection.findOne("user", event.getMember().getIdLong());
+        if (booster != null) booster.removePasses(eventAlerts);
+    }
+}
